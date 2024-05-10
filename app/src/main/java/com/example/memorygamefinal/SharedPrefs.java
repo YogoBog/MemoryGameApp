0ec8a3b190bfd8956e91df939ab9714064061f52
YoavG <yoavgold99@gmail.com>
2024-05-10 19:20:45 +0300

    Update SharedPrefs.java

--- app/src/main/java/com/example/memorygamefinal/SharedPrefs.java
+++ app/src/main/java/com/example/memorygamefinal/SharedPrefs.java
@@ -14,19 +14,33 @@ import java.util.Map;
 
 public class SharedPrefs {
 
+    // SharedPreferences instance to handle data persistence
     private static SharedPreferences sharedPreferences;
+
+    // Map to store user data
     private static Map<String, UserData> users;
+
+    // Key for storing the username of the last signed-in user
     private static final String LAST_SIGNED_IN_USER = "last_signed_in_user";
 
+    // Constructor for SharedPrefs class
+    public SharedPrefs(Context context) {
+        sharedPreferences = context.getSharedPreferences("userData", Context.MODE_PRIVATE);
+        users = new HashMap<>();
+        loadData(); // Load user data from SharedPreferences when the SharedPrefs object is created
+    }
 
+    // Method to retrieve the map of users
     public Map<String, UserData> getUsers() {
         return users;
     }
 
+    // Method to retrieve UserData object for a specific username
     public UserData getUserData(String username) {
         return users.get(username);
     }
 
+    // Method to retrieve password for a specific username
     public String getPassword(String username) {
         if (users.containsKey(username)) {
             UserData userData = users.get(username);
@@ -35,10 +49,12 @@ public class SharedPrefs {
         return null;
     }
 
+    // Method to retrieve the username of the last signed-in user
     public static String getLastSignedInUser() {
         return sharedPreferences.getString(LAST_SIGNED_IN_USER, null);
     }
 
+    // Method to set score for a specific user
     public void setScore(String username, int score) {
         if (users.containsKey(username)) {
             UserData userData = users.get(username);
@@ -46,6 +62,7 @@ public class SharedPrefs {
         }
     }
 
+    // Method to retrieve score for a specific user
     public static int getScore(String username) {
         if (users.containsKey(username)) {
             UserData userData = users.get(username);
@@ -54,6 +71,7 @@ public class SharedPrefs {
         return 0;
     }
 
+    // Method to load user data from SharedPreferences
     private void loadData() {
         Map<String, ?> allEntries = sharedPreferences.getAll();
         for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
@@ -68,7 +86,7 @@ public class SharedPrefs {
         }
     }
 
-
+    // Method to save user data to SharedPreferences
     public void saveData() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         for (UserData userData : users.values()) {
@@ -78,12 +96,7 @@ public class SharedPrefs {
         editor.apply();
     }
 
-    public SharedPrefs(Context context) {
-        sharedPreferences = context.getSharedPreferences("userData", Context.MODE_PRIVATE);
-        users = new HashMap<>();
-        loadData();
-    }
-
+    // Method to register a new user
     public boolean registerUser(Context context, String username, String password) {
         if (!users.containsKey(username)) {
             UserData userData = new UserData();
@@ -91,37 +104,17 @@ public class SharedPrefs {
             userData.setPassword(password);
             userData.setScore(0);
             users.put(username, userData);
-            saveData();
+            saveData(); // Save user data after registration
             return true;
         }
-        Toast.makeText(context, "An account with this usarename already exist", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "An account with this username already exists", Toast.LENGTH_SHORT).show();
         return false;
     }
 
+    // Method to login a user
     public boolean loginUser(String username, String password) {
         if (users.containsKey(username)) {
             UserData userData = users.get(username);
             if (userData.getPassword().equals(password)) {
-                storeLastSignedInUser(username);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void storeLastSignedInUser(String username) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(LAST_SIGNED_IN_USER, username);
-        editor.apply();
-    }
-
-    public void logoutUser() {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove(LAST_SIGNED_IN_USER);
-        editor.apply();
-    }
-
-}
-
-
-
+                storeLastSignedInUser(username); // Store the username of the signed-in user
+      

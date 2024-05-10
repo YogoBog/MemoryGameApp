532ba112fe642a323ca63666f5f82ff12f999abc
YoavG <yoavgold99@gmail.com>
2024-05-10 19:16:41 +0300

    Update Pics.java

--- app/src/main/java/com/example/memorygamefinal/Pics.java
+++ app/src/main/java/com/example/memorygamefinal/Pics.java
@@ -3,37 +3,44 @@ package com.example.memorygamefinal;
 import java.util.Random;
 
 public class Pics {
+    // Member variable to store pictures
     private int[] Pics;
 
+    // Constructor to initialize Pics array with random values
     public Pics() {
-
-        Random rd = new Random();
-        int num;
-        this.Pics = new int[8];
-        int[] usedPics = new int[4];
+        Random rd = new Random(); // Random object to generate random numbers
+        int num; // Variable to store random number
+        this.Pics = new int[8]; // Initialize Pics array with size 8
+        int[] usedPics = new int[4]; // Array to keep track of used pictures
+        // Loop to fill Pics array with random picture indices
         for (int i = 0; i < 8; i++) {
-            num = rd.nextInt(4);
+            num = rd.nextInt(4); // Generate a random number between 0 and 3
+            // Ensure each picture is used only twice
             while (usedPics[num] == 2)
-                num = rd.nextInt(4);
-            usedPics[num]++;
-            this.Pics[i] = num + 1;
+                num = rd.nextInt(4); // Generate a new random number if the current one is already used twice
+            usedPics[num]++; // Increment the count of the chosen picture
+            this.Pics[i] = num + 1; // Store the picture index in the Pics array
         }
     }
 
+    // Method to get the Pics array
     public int[] getPics() {
         return this.Pics;
     }
 
+    // Method to generate a new order of pictures
     public void newOrder() {
-        Random rd = new Random();
-        int num;
-        int[] usedPics = new int[4];
+        Random rd = new Random(); // Random object to generate random numbers
+        int num; // Variable to store random number
+        int[] usedPics = new int[4]; // Array to keep track of used pictures
+        // Loop to generate a new order of pictures
         for (int i = 0; i < 8; i++) {
-            num = rd.nextInt(4);
+            num = rd.nextInt(4); // Generate a random number between 0 and 3
+            // Ensure each picture is used only twice
             while (usedPics[num] == 2)
-                num = rd.nextInt(4);
-            usedPics[num]++;
-            this.Pics[i] = num + 1;
+                num = rd.nextInt(4); // Generate a new random number if the current one is already used twice
+            usedPics[num]++; // Increment the count of the chosen picture
+            this.Pics[i] = num + 1; // Store the picture index in the Pics array
         }
     }
 }
